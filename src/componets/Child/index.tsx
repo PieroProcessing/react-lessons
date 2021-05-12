b16af55f7a23baef78fc117e8746ddf057e93f39
PieroProcessing <piero.dauber@gmail.com>
2021-05-12 12:22:37 +0200

    useState

--- src/componets/Child/index.tsx
+++ src/componets/Child/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, MouseEvent } from "react";
 
 interface Props {
   [key: string]: any;
@@ -8,8 +8,15 @@ interface Props {
 
 const Child = (props: Props): JSX.Element => {
     console.log("i'm child", props.element.id);
+    const [click, setClick] = useState<string>()
+
+    const handleClick = (event: MouseEvent<HTMLDivElement>): void => {
+      console.log("i have clicked", event);
+      setClick(event.currentTarget.innerHTML);
+      event.currentTarget.classList.add('newclass');
+    };
     return (
-      <div id={props.element.id} className='col py-5 border' style={{ backgroundColor: props.backgroundColor}}>
+      <div id={props.element.id} className='col py-5 border' onClick={handleClick} style={{ backgroundColor: props.backgroundColor}}>
         i'm child. My name is {props.element.id}.
         {props?.variableInParent || null}
       </div>
--- src/componets/ParentSibling/index.tsx
+++ src/componets/ParentSibling/index.tsx
@@ -1,34 +1,52 @@
-import React from "react";
+import React, { ChangeEvent,   useState } from "react";
 import Child from "../Child";
 
 interface Props {
   [key: string]: any;
-  callback: (attr: any)=> void
+  callback: (attr: any) => void;
+  children: ((attr: any) => JSX.Element);
 }
 
-const ParentSibling = (props: Props): JSX.Element =>{
-  console.log('i\'m parent  sibling', props)
-  
+const ParentSibling: React.FC<Props> = (
+  props
+): JSX.Element => {
+  console.log("i'm parent  sibling", props);
+  const [input, setInput] = useState<{ name: string; value: string }>();
+  const handleChange = ({
+    target: { name, value },
+  }: ChangeEvent<HTMLInputElement>): void => {
+    console.log("input usefull value", name, value);
+    setInput({ name, value });
+  };
   return (
     <div className="border">
       <div className="p-y5 ">
         <span onClick={() => props.callback("I'm parent sibling")}>
           I'm parent sibling
         </span>
+        <div>
+          <label>I'm an usefull input </label>
+          <input
+            onChange={handleChange}
+            name="usefullinput"
+            value={input?.value || ""}
+            // value={input?.value}
+          />
+        </div>
         <div className="d-flex flex-row">
           <Child
             isActive={true}
             element={{ id: "This is Child inside", age: 46 }}
           />
-          {props.children({
+          {props?.children({
             isActive: true,
-            element: { id: "this is with clone", age: 46 },
+            element: { id: `${input?.value} this is Child with clone`, age: 46 },
             backgroundColor: "red",
-          })}        
+          })}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default ParentSibling;

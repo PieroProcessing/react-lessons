57f8399edce96be847238e3981761cc85563ded3
PieroProcessing <piero.dauber@gmail.com>
2021-09-06 17:13:51 +0200

    husky setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { RHookForm } from './componets/ReactHookForm';
 
 const App = (): JSX.Element => {
-  console.log('')
   return (
     <div>
-      <RHookForm /> 
+      <RHookForm />
     </div>
   );
 };
